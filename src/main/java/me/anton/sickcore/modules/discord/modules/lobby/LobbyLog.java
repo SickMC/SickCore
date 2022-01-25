2ecd2c652849ae08233d36a8af9c3b56dbe8b84f
4NI0N <anton.wiblishauser@gmx.de>
2022-01-25 20:06:16 +0100

    fix LobbyLog.java

--- src/main/java/me/anton/sickcore/modules/discord/modules/lobby/LobbyLog.java
+++ src/main/java/me/anton/sickcore/modules/discord/modules/lobby/LobbyLog.java
@@ -2,9 +2,6 @@ package me.anton.sickcore.modules.discord.modules.lobby;
 
 import me.anton.sickcore.api.utils.discord.DiscordIds;
 import me.anton.sickcore.modules.discord.DiscordModule;
-import me.anton.sickcore.modules.discord.handlers.messages.DiscordMessages;
-import me.anton.sickcore.modules.discord.handlers.messages.EmbedType;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -16,7 +13,7 @@ public class LobbyLog extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-        MessageEmbed embed = new me.anton.sickcore.modules.discord.handlers.messages.EmbedBuilder(EmbedType.UTILITY, event.getMember())
+        MessageEmbed embed = new me.anton.sickcore.modules.discord.handlers.messages.EmbedBuilder(event.getMember())
                 .setTitle("Join")
                 .setContent(event.getMember().getAsMention() + " joined the server! :partying_face:").build();
         DiscordModule.getInstance().getMainGuild().getTextChannelById(DiscordIds.lobbyChannel).sendMessageEmbeds(embed).queue();
