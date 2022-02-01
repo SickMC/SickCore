90eeafcc7b484870b8afcfdede8ca515de707c1b
4NI0N <anton.wiblishauser@gmx.de>
2022-02-01 21:45:18 +0100

    add ColorUtils.java

---
+++ src/main/java/me/anton/sickcore/api/utils/common/ColorUtils.java
@@ -0,0 +1,24 @@
+package me.anton.sickcore.api.utils.common;
+
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.md_5.bungee.api.ChatColor;
+
+import java.awt.*;
+
+public class ColorUtils {
+
+    public static TextColor toTextColor(Color color){
+        return TextColor.color(color.getRGB());
+    }
+
+    public static ChatColor toBungeeColor(Color color){
+        return ChatColor.of(color);
+    }
+
+    public static NamedTextColor toNamedTextColor(org.bukkit.ChatColor color){
+        TextColor color1 = TextColor.color(color.asBungee().getColor().getRed(), color.asBungee().getColor().getGreen(), color.asBungee().getColor().getBlue());
+        return NamedTextColor.nearestTo(color1);
+    }
+
+}
