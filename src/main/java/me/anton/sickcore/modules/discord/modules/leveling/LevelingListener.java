0a44364e87fd8edf0324bae5874ed81d2c450e53
4NI0N <anton.wiblishauser@gmx.de>
2022-02-06 19:19:29 +0100

    add Voice Leveling

--- src/main/java/me/anton/sickcore/modules/discord/modules/leveling/LevelingListener.java
+++ src/main/java/me/anton/sickcore/modules/discord/modules/leveling/LevelingListener.java
@@ -6,6 +6,8 @@ import me.anton.sickcore.api.player.apiPlayer.provider.DiscordAPIPlayerAdapter;
 import me.anton.sickcore.api.utils.discord.DiscordIds;
 import me.anton.sickcore.modules.discord.DiscordModule;
 import me.anton.sickcore.modules.discord.handlers.messages.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
@@ -17,6 +19,7 @@ public class LevelingListener extends ListenerAdapter {
 
     private DatabaseModel model = DiscordModule.getInstance().getGamePlayer();
     private String[] allowed = {DiscordIds.chat, DiscordIds.suggestionchannel, DiscordIds.bugschannel, DiscordIds.supportchannel};
+    private String[] allowedChannels = {DiscordIds.talk1, DiscordIds.talk2, DiscordIds.talk3};
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
@@ -36,13 +39,46 @@ public class LevelingListener extends ListenerAdapter {
         for (DiscordLevel value : DiscordLevel.values()) {
             if (oldPoint == value.getEnd() && oldPoint + 1 == value.getNext().getStart()){
                 if (value.getNext().getReward().expire() == -1){
-                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent("You gain a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
                             + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "** (permanent)").build()).queue();
                 }else if (value.getNext().getReward().expire() == 0){
-                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent("You gain a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
                             + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "**").build()).queue();
                 }else {
-                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent("You gain a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                    event.getMessage().replyEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                            + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "** (" + value.getNext().getReward().expire() + " days)").build()).queue();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+        if (event.getMember().getUser().isBot())return;
+        if (!Arrays.asList(allowedChannels).contains(event.getChannelJoined().getId()))return;
+
+        if (!model.documentExists(Finder.stringFinder("userID", event.getMember().getUser().getId()))){
+            if (DiscordAPIPlayerAdapter.isVerified(event.getMember().getUser().getId()))model.createDocument(new Document("userID", event.getMember().getUser().getId()).append("points", 5).append("enabled", true));
+            else model.createDocument(new Document("userID", event.getMember().getUser().getId()).append("points", 5).append("enabled", false));
+        }
+        Document player = model.getDocument(Finder.stringFinder("userID", event.getMember().getUser().getId()));
+        int oldPoint = player.getInteger("points");
+
+        player.replace("points", oldPoint + 5);
+
+        model.updateDocument(Finder.stringFinder("userID", event.getMember().getUser().getId()), player);
+
+        for (DiscordLevel value : DiscordLevel.values()) {
+            if (oldPoint == value.getEnd() && oldPoint + 1 == value.getNext().getStart()){
+                TextChannel channel = DiscordModule.getInstance().getMainGuild().getTextChannelById(DiscordIds.chat);
+                if (value.getNext().getReward().expire() == -1){
+                    channel.sendMessageEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                            + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "** (permanent)").build()).queue();
+                }else if (value.getNext().getReward().expire() == 0){
+                    channel.sendMessageEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
+                            + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "**").build()).queue();
+                }else {
+                    channel.sendMessageEmbeds(new EmbedBuilder(event.getMember()).setTitle("Level Up").mention(event.getMember()).setContent(event.getMember().getAsMention() + "\nYou gained a level up from **" + value.getName() + "** to **" + value.getNext().getName() + "**!"
                             + "\nYour reward is a " + value.getNext().getReward().getName() + ":\n >>> **" + value.getNext().getReward().getType() + ": " + value.getNext().getReward().getValue() + "** (" + value.getNext().getReward().expire() + " days)").build()).queue();
                 }
             }
