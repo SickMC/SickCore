042529b41824c22d7c05ae112e3cde50e0f8fc99
4NI0N <anton.wiblishauser@gmx.de>
2022-02-12 23:08:07 +0100

    add UUID Finder to Finder

--- src/main/java/me/anton/sickcore/api/database/Finder.java
+++ src/main/java/me/anton/sickcore/api/database/Finder.java
@@ -6,20 +6,26 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bson.conversions.Bson;
 
+import java.util.UUID;
+
 @Data
 @Getter
 @Setter
-public class Finder {
+public class Finder{
 
     private final String key;
     private final Object value;
 
-    public Bson bson(){
+    public Bson bson() {
         return Filters.eq(key, value);
     }
 
-    public static Finder stringFinder(String key, String value){
+    public static Finder stringFinder(String key, String value) {
         return new Finder(key, value);
+    }
+
+    public static Finder uuidFinder(String key, UUID value) {
+        return new Finder(key, value.toString());
     }
 
 }
