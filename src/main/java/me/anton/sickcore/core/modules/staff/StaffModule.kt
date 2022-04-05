39970f4b06a1d0b84318de9c69777e74def92c29
4NI0N <anton.wiblishauser@gmx.de>
2022-04-06 00:12:09 +0200

    add StaffModule
    Took 2 hours 45 minutes

---
+++ src/main/java/me/anton/sickcore/core/modules/staff/StaffModule.kt
@@ -0,0 +1,27 @@
+package me.anton.sickcore.core.modules.staff
+
+import me.anton.sickcore.core.Environment
+
+class StaffModule : me.anton.sickcore.core.modules.Module(){
+
+    companion object{
+        lateinit var instance: StaffModule
+    }
+
+    override val name: String
+        get() = "Staff"
+
+    val staff = Staff()
+
+    override suspend fun start() {
+        instance = this
+        when(environment){
+            Environment.VELOCITY ->{
+                BuildServerCommand().register()
+            }
+        }
+    }
+
+    override suspend fun shutdown() {
+    }
+}
---
+++ src/main/java/me/anton/sickcore/core/modules/staff/StaffPlayer.kt
@@ -0,0 +1,62 @@
+package me.anton.sickcore.core.modules.staff
+
+import me.anton.sickcore.core.player.SickPlayer
+import me.anton.sickcore.core.player.SickPlayers
+import me.anton.sickcore.utils.mongo.MongoCollection
+import me.anton.sickcore.utils.mongo.MongoDocument
+import org.bson.Document
+import java.util.*
+import kotlin.collections.HashMap
+
+class StaffPlayer(val uuid: UUID,val document: MongoDocument) {
+
+    val notifyConfig = document.document.get("notify", Document::class.java)
+    val joined = document.document.getLong("joined")
+    val cooldown = document.document.getLong("cooldown")
+    val warns = document.document.getInteger("warns")
+
+    suspend fun getSickPlayer(): SickPlayer{
+        return SickPlayers.getSickPlayer(uuid)!!
+    }
+
+}
+
+class StaffPlayers{
+
+    companion object{
+        val collection = MongoCollection("staff")
+        val players = HashMap<UUID, StaffPlayer>()
+
+        suspend fun getStaffPlayer(uuid: UUID): StaffPlayer?{
+            if (players.contains(uuid))return players[uuid]
+            else{
+                if (collection.getDocument("uuid", uuid.toString()) == null)return null
+                val sickPlayer = StaffPlayer(uuid, collection.getDocument("uuid", uuid.toString())!!)
+                players[uuid] = sickPlayer
+                return sickPlayer
+            }
+        }
+
+        suspend fun reloadPlayer(uuid: UUID): StaffPlayer?{
+            if (players.contains(uuid)) players.remove(uuid)
+            return getStaffPlayer(uuid)
+        }
+
+        suspend fun createPlayer(uuid: UUID): StaffPlayer {
+            if (collection.getDocument("uuid", uuid.toString()) != null)return getStaffPlayer(uuid)!!
+            val playerDoc = Document("uuid", uuid.toString())
+                .append("joined", System.currentTimeMillis())
+                .append("warns", 0)
+                .append("notify", Document("service", "false")
+                    .append("flags", "false")
+                    .append("joininfo", "false")
+                    .append("punishments", "false")
+                    .append("report", "false")
+                    .append("teamchat", "false"))
+                .append("cooldown", 0L)
+            collection.createDocument(playerDoc)
+            return getStaffPlayer(uuid)!!
+        }
+    }
+
+}
