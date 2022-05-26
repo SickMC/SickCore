a380bc6cc13ff61c6f9d768aecdb383de6382770
4NI0N <anton.wiblishauser@gmx.de>
2022-05-27 01:54:11 +0200

    Update build.gradle.kts
    Took 8 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories{
 publishing {
     repositories {
         maven {
-            name = "SickCore"
+            name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/SickMC/SickCore")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
