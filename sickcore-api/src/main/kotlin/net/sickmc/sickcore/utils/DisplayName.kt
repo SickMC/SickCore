805bd1b24815f7a7255b770126a4d0062b0946c6
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-01 20:27:09 +0200

    invent mutableComponent

--- sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/DisplayName.kt
+++ sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/DisplayName.kt
@@ -1,5 +1,5 @@
 package net.sickmc.sickcore.utils
 
-import net.minecraft.network.chat.BaseComponent
+import net.minecraft.network.chat.MutableComponent
 
-data class DisplayName(val full: BaseComponent, val kyoriFull: net.kyori.adventure.text.Component, val prefix: String, val name: String, val color: Int)
+data class DisplayName(val full: MutableComponent, val kyoriFull: net.kyori.adventure.text.Component, val prefix: String, val name: String, val color: Int)
--- sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/fabric/PlayerExtensions.kt
+++ sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/fabric/PlayerExtensions.kt
@@ -3,10 +3,11 @@ package net.sickmc.sickcore.utils.fabric
 import net.minecraft.network.chat.ChatSender
 import net.minecraft.network.chat.ChatType
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.network.chat.PlayerChatMessage
 import net.minecraft.server.level.ServerPlayer
 import java.util.UUID
 
-fun ServerPlayer.sendMessage(message: Component){
+fun ServerPlayer.sendMessage(message: MutableComponent){
     this.sendChatMessage(PlayerChatMessage.unsigned(message), ChatSender(UUID.randomUUID(), Component.empty()), ChatType.CHAT)
 }
