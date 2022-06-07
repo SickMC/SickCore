1931bac90b00e32b189241522fb3a58b106f1d3c
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-07 17:17:17 +0200

    create useful extensions
    Took 5 minutes

---
+++ sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/fabric/LocationExtensions.kt
@@ -0,0 +1,8 @@
+package net.sickmc.sickcore.utils.fabric
+
+import net.minecraft.world.phys.Vec3
+import kotlin.math.roundToInt
+
+fun Vec3.toPrettyString(): String{
+    return "(${this.x.roundToInt()}, ${this.y.roundToInt()}, ${this.x.roundToInt()})"
+}
--- sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/fabric/PlayerExtensions.kt
+++ sickcore-api/src/main/kotlin/net/sickmc/sickcore/utils/fabric/PlayerExtensions.kt
@@ -6,8 +6,17 @@ import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.MutableComponent
 import net.minecraft.network.chat.PlayerChatMessage
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.player.Player
+import net.sickmc.sickcore.commonPlayer.SickPlayer
+import net.sickmc.sickcore.commonPlayer.SickPlayers
 import java.util.UUID
 
 fun ServerPlayer.sendMessage(message: MutableComponent){
     this.sendChatMessage(PlayerChatMessage.unsigned(message), ChatSender(UUID.randomUUID(), Component.empty()), ChatType.CHAT)
 }
+
+val ServerPlayer.sickPlayer: SickPlayer?
+    get() = SickPlayers.instance.getCachedEntity(this.uuid)
+
+val Player.sickPlayer: SickPlayer?
+    get() = SickPlayers.instance.getCachedEntity(this.uuid)
