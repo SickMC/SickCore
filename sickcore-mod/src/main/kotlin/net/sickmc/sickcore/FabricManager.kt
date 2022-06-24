409c178d8152f206ff632250e7608d564ec8a267
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-25 00:06:03 +0200

    change mod startup
    Took 2 minutes

--- sickcore-mod/src/main/kotlin/net/sickmc/sickcore/FabricManager.kt
+++ sickcore-mod/src/main/kotlin/net/sickmc/sickcore/FabricManager.kt
@@ -7,13 +7,15 @@ import kotlinx.coroutines.launch
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
+import net.minecraft.server.MinecraftServer
 import net.sickmc.sickcore.commonPlayer.SickPlayers
 import net.sickmc.sickcore.games.Game
 import net.sickmc.sickcore.games.survival.SurvivalPlayers
-import net.sickmc.sickcore.utils.EventManager
+import net.sickmc.sickcore.games.survival.preGen
 import net.sickmc.sickcore.utils.mongo.databaseScope
 
 val fabricScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+lateinit var server: MinecraftServer
 
 class FabricManager : ModInitializer {
 
@@ -25,13 +27,26 @@ class FabricManager : ModInitializer {
         instance = this
 
         val moduleHandler = ModuleHandler(environment)
-        databaseScope.launch {
-            registerCommands()
-            registerCaches()
-            EventManager.register()
-            moduleHandler.start()
+
+        registerCaches()
+
+        fabricScope.launch {
             handleSickPlayers()
-            Game.enable()
+            moduleHandler.start()
+        }
+
+        registerCommands()
+        registerCaches()
+        Game.preEnable()
+
+        ServerLifecycleEvents.SERVER_STARTED.register{
+            server = it
+            fabricScope.launch {
+                Game.enable()
+            }
+            //handleTablist()
+            it.commands.performCommand(it.createCommandSourceStack(), "chunky continue")
+            preGen = true
         }
 
         ServerLifecycleEvents.SERVER_STOPPING.register{
@@ -50,12 +65,22 @@ class FabricManager : ModInitializer {
         SickPlayers()
     }
 
-    private suspend fun handleSickPlayers(){
+    private fun handleSickPlayers(){
         ServerPlayConnectionEvents.INIT.register{ handler, _->
             if (handler.player == null)return@register
             databaseScope.launch {
                 SickPlayers.instance.reloadEntity(handler.player!!.uuid)
             }
+        }
+    }
+
+    private fun handleTablist(){
+        ServerPlayConnectionEvents.JOIN.register{ _, _, _ ->
+            Game.current.tablist.reloadAll()
+        }
+
+        ServerPlayConnectionEvents.DISCONNECT.register{ _, _ ->
+            Game.current.tablist.reloadAll()
         }
     }
 
