9e42de453718dc17174b86c05094e17fc6e726d9
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-06-29 21:32:36 +0200

    finally fix telekinesis
    Took 4 hours 56 minutes

--- sickcore-mod/src/main/java/net/sickmc/sickcore/mixin/LivingEntityMixin.java
+++ sickcore-mod/src/main/java/net/sickmc/sickcore/mixin/LivingEntityMixin.java
@@ -4,36 +4,57 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.sickmc.sickcore.games.Game;
+import net.sickmc.sickcore.games.survival.Survival;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
     @Shadow
-    public abstract ResourceLocation getLootTable();
-
-    @Shadow
     protected abstract LootContext.Builder createLootContext(boolean causedByPlayer, DamageSource source);
 
-    @Inject(method = "dropFromLootTable", at = @At("RETURN"))
-    public void telekinesisDrops(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
-        if (!causedByPlayer) return;
-        Player player = (Player) source.getEntity();
+    @Inject(method = "dropFromLootTable", at = @At(value = "HEAD"), cancellable = true)
+    public void telekinesisLoot(DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
-        ResourceLocation resourceLocation = getLootTable();
+        if (entity instanceof Player)
+            return;
+        ResourceLocation resourceLocation = entity.getLootTable();
         LootTable lootTable = entity.level.getServer().getLootTables().get(resourceLocation);
-        LootContext.Builder builder = createLootContext(true, source);
+        LootContext.Builder builder = createLootContext(causedByPlayer, source);
         lootTable.getRandomItems(builder.create(LootContextParamSets.ENTITY), item -> {
-            if (!player.getInventory().add(item)) entity.spawnAtLocation(item);
+            if (item.isEmpty()) return;
+            if (!causedByPlayer || !Game.current.getName().equals("Survival")) {
+                entity.spawnAtLocation(item);
+                ci.cancel();
+                return;
+            }
+            Player player = (Player) source.getEntity();
+            Survival survival = (Survival) Game.current;
+            boolean telekinesis = false;
+            for (Enchantment enchantment : survival.getTelekinesis())
+                if (EnchantmentHelper.getEnchantments(player.getMainHandItem()).containsKey(enchantment)) {
+                    telekinesis = true;
+                    break;
+                }
+
+            if (!telekinesis) {
+                entity.spawnAtLocation(item);
+                ci.cancel();
+                return;
+            }
+            if (!player.getInventory().add(item)) {
+                entity.spawnAtLocation(item);
+            }
+            ci.cancel();
         });
     }
-
-
 }
