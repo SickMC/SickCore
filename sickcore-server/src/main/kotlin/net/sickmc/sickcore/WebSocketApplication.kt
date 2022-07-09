09cda87d5eb0be01e416d1b1c33c6cf345838d3a
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-07-09 20:46:33 +0200

    try to fix websockets
    Took 43 minutes

--- sickcore-server/src/main/kotlin/net/sickmc/sickcore/WebSocketApplication.kt
+++ sickcore-server/src/main/kotlin/net/sickmc/sickcore/WebSocketApplication.kt
@@ -6,8 +6,6 @@ import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.util.collections.*
-import io.ktor.websocket.*
-import java.time.Duration
 import java.util.*
 
 fun main() {
@@ -15,10 +13,7 @@ fun main() {
         Netty, port = System.getenv("WEBSOCKET_PORT").toInt(), host = System.getenv("WEBSOCKET_SERVER_ADDRESS")
     ) {
         install(WebSockets) {
-            pingPeriod = Duration.ofSeconds(15)
-            timeout = Duration.ofSeconds(15)
-            maxFrameSize = Long.MAX_VALUE
-            masking = false
+            timeoutMillis = Long.MAX_VALUE
         }
 
         routing {
@@ -26,19 +21,19 @@ fun main() {
             val verifyConnections = Collections.synchronizedSet<DefaultWebSocketServerSession>(ConcurrentSet())
 
             webSocket("/event") {
+                eventConnections.add(this)
                 for (frame in incoming) {
-                    println(frame)
-                    eventConnections.add(this)
                     eventConnections.forEach { it.send(frame) }
-                    println("Sent!!")
                 }
             }
             webSocket("/verify") {
+                verifyConnections.add(this)
                 for (frame in incoming) {
                     println(frame)
-                    verifyConnections.add(this)
-                    verifyConnections.forEach { it.send(frame) }
-                    println("Sent!!")
+                    for (connection in verifyConnections) {
+                        connection.send(frame)
+                        println("Sent!!")
+                    }
                 }
             }
         }
