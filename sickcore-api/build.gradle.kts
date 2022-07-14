242d0c1a721e29e0ececf5f8b8e5daf3f039bdf5
4NI0N <79146501+btwonion@users.noreply.github.com>
2022-07-15 00:01:30 +0200

    add publish script
    Took 2 minutes

--- sickcore-api/build.gradle.kts
+++ sickcore-api/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
@@ -5,5 +7,21 @@ plugins {
     `mongo-build-script`
     `mod-build-script`
     `websocket-client-script`
+    `maven-publish`
+}
+
+publishing {
+
+    repositories{
+        maven {
+            name = "SickCore-API"
+            url = URI("https://maven.pkg.github.com/sickmc/sickcore")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
 }
 
