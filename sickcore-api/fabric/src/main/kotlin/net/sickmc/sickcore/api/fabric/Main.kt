8108a5544872ddb17378621f0517a9ef44cc94c2
Anton <anton.wiblishauser@gmx.de>
2022-09-27 21:52:23 +0200

    add top level server variable

--- sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/Main.kt
+++ sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/Main.kt
@@ -1,8 +1,12 @@
 package net.sickmc.sickcore.api.fabric
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import net.minecraft.server.MinecraftServer
 import net.sickmc.sickapi.SickPlayer
 import net.sickmc.sickapi.playerCache
 import net.sickmc.sickapi.rank.loadRanks
@@ -11,11 +15,16 @@ import net.sickmc.sickapi.util.initMongo
 import net.sickmc.sickapi.util.players
 import net.silkmc.silk.core.event.Events
 import net.silkmc.silk.core.event.Player
+import net.silkmc.silk.core.event.Server
 import org.litote.kmongo.eq
 import java.util.*
 
+lateinit var modScope: CoroutineScope
+lateinit var server: MinecraftServer
+
 @Suppress("unused")
 fun init() {
+    modScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
     databaseScope.launch {
         initMongo()
         loadRanks()
@@ -28,8 +37,11 @@ object FabricEntrypoint {
     private val playTimeMutex = Mutex()
 
     fun init() {
+        serverStartListener
         playerListener
     }
+
+    private val serverStartListener = Events.Server.preStart.listen { event -> server = event.server }
 
     private val playerListener = Events.Player.preLogin.listen { event ->
         databaseScope.launch {
