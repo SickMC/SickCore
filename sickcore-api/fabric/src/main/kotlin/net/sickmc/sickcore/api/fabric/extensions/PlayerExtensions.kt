5adab69002968082a1c8c3727a812056f756d38c
Anton <anton.wiblishauser@gmx.de>
2022-09-29 19:12:22 +0200

    add player/sickplayer extensions

--- sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/extensions/PlayerExtensions.kt
+++ sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/extensions/PlayerExtensions.kt
@@ -1,6 +1,8 @@
 package net.sickmc.sickcore.api.fabric.extensions
 
+import net.minecraft.server.level.ServerPlayer
 import net.sickmc.sickapi.SickPlayer
+import net.sickmc.sickapi.playerCache
 import net.sickmc.sickapi.rank.parent
 import net.sickmc.sickcore.api.fabric.server
 import net.silkmc.silk.core.text.literalText
@@ -9,3 +11,5 @@ val SickPlayer.displayName
     get() = literalText(
         server.playerList.getPlayer(this.uuid)?.name?.string ?: error("requesting displayname of null player")
     ) { color = currentRank.parent.color.fallbackColor }
+
+suspend fun ServerPlayer.sickPlayer(): SickPlayer? = playerCache.get(this.uuid)
