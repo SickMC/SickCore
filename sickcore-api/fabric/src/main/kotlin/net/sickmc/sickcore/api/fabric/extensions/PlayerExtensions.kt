530f4825606687506e311d089223fb544285eb29
Anton <anton.wiblishauser@gmx.de>
2022-10-24 20:02:28 +0200

    make SickPlayer#displayname nullable

--- sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/extensions/PlayerExtensions.kt
+++ sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/extensions/PlayerExtensions.kt
@@ -1,5 +1,6 @@
 package net.sickmc.sickcore.api.fabric.extensions
 
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.server.level.ServerPlayer
 import net.sickmc.sickapi.SickPlayer
 import net.sickmc.sickapi.playerCache
@@ -7,9 +8,12 @@ import net.sickmc.sickapi.rank.parent
 import net.sickmc.sickcore.api.fabric.server
 import net.silkmc.silk.core.text.literalText
 
-val SickPlayer.displayName
-    get() = literalText(
-        server.playerList.getPlayer(this.uuid)?.name?.string ?: error("requesting displayname of null player")
-    ) { color = currentRank.parent.color.fallbackColor }
+val SickPlayer.displayName: MutableComponent?
+    get() {
+        val player = server.playerList.getPlayer(this.uuid) ?: return null
+        return literalText(
+            player.name.string
+        ) { color = currentRank.parent.color.fallbackColor }
+    }
 
 suspend fun ServerPlayer.sickPlayer(): SickPlayer? = playerCache.get(this.uuid)
