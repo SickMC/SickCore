8a42f807a64311766c58de19b34e0182b820c71a
Anton <anton.wiblishauser@gmx.de>
2022-11-12 15:30:26 +0100

    improve playTime command

--- sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/commands/PlaytimeCommand.kt
+++ sickcore-api/fabric/src/main/kotlin/net/sickmc/sickcore/api/fabric/commands/PlaytimeCommand.kt
@@ -1,19 +1,20 @@
 package net.sickmc.sickcore.api.fabric.commands
 
-import net.minecraft.commands.arguments.EntityArgument
+import net.minecraft.commands.arguments.GameProfileArgument
 import net.sickmc.sickapi.SickPlayer
 import net.sickmc.sickapi.util.Colors
 import net.sickmc.sickapi.util.players
+import net.sickmc.sickcore.api.fabric.extensions.sickPlayer
 import net.silkmc.silk.commands.command
 import net.silkmc.silk.core.text.literalText
 import org.litote.kmongo.eq
 import kotlin.time.Duration.Companion.milliseconds
 
 val playtimeCommand = command("playtime") {
-    argument("player", EntityArgument.player()) { targetPl ->
+    argument("player", GameProfileArgument.gameProfile()) { targetPl ->
         runsAsync {
             val player = source.playerOrException
-            val target = players.findOne(SickPlayer::uuidString eq targetPl().findSinglePlayer(source).stringUUID)
+            val target = players.findOne(SickPlayer::uuidString eq targetPl().getNames(source).first().id.toString())
             if (target == null) {
                 player.sendSystemMessage(literalText("This player cannot be found!") { color = Colors.darkRed }, true)
                 return@runsAsync
@@ -24,7 +25,7 @@ val playtimeCommand = command("playtime") {
 
     runsAsync {
         val player = source.playerOrException
-        player.sendSystemMessage(literalText(players.findOne(SickPlayer::uuidString eq player.stringUUID)?.playtime?.milliseconds.toString()) {
+        player.sendSystemMessage(literalText(player.sickPlayer()?.playtime?.milliseconds.toString()) {
             color = 0xF99147
         })
     }
